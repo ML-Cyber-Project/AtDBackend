be37f1004cb91ea9c38ee0de92350db8a72599b9
Mathu-lmn <80094438+Mathu-lmn@users.noreply.github.com>
2024-12-11 23:15:27 -0500

    add function to create samples of data for the API

--- test.py
+++ test.py
@@ -43,11 +43,62 @@ model = mlflow.sklearn.load_model("runs:/c0147219bacd4e92bb09477869eb452d/model"
 
 LABELS_NUM = ["BENIGN", "DoS"]
 
+COLUMN_MAPPINGS = {
+    "flow_duration": "Flow Duration",
+    "fwd_packet_length_std": "Fwd Packet Length Std",
+    "bwd_packet_length_mean": "Bwd Packet Length Mean",
+    "bwd_packet_length_std": "Bwd Packet Length Std",
+    "flow_bytes_s": "Flow Bytes/s",
+    "flow_packets_s": "Flow Packets/s",
+    "flow_iat_mean": "Flow IAT Mean",
+    "flow_iat_std": "Flow IAT Std",
+    "flow_iat_max": "Flow IAT Max",
+    "fwd_iat_total": "Fwd IAT Total",
+    "fwd_iat_mean": "Fwd IAT Mean",
+    "fwd_iat_std": "Fwd IAT Std",
+    "fwd_iat_max": "Fwd IAT Max",
+    "bwd_iat_total": "Bwd IAT Total",
+    "bwd_iat_mean": "Bwd IAT Mean",
+    "bwd_iat_std": "Bwd IAT Std",
+    "bwd_iat_max": "Bwd IAT Max",
+    "fwd_packets_s": "Fwd Packets/s",
+    "bwd_packets_s": "Bwd Packets/s",
+    "packet_length_mean": "Packet Length Mean",
+    "packet_length_std": "Packet Length Std",
+    "packet_length_variance": "Packet Length Variance",
+    "average_packet_size": "Average Packet Size",
+    "avg_bwd_segment_size": "Avg Bwd Segment Size",
+    "active_mean": "Active Mean",
+    "active_std": "Active Std",
+    "active_max": "Active Max",
+    "active_min": "Active Min",
+    "idle_mean": "Idle Mean",
+    "idle_std": "Idle Std",
+    "idle_max": "Idle Max",
+    "idle_min": "Idle Min"
+}
+
+# revert the column mappings
+COLUMN_MAPPINGS = {v: k for k, v in COLUMN_MAPPINGS.items()}
+
 # Load data/features_cleaned.csv and data/labels_cleaned.csv
 features = pd.read_csv("data/features_cleaned.csv")
 labels = pd.read_csv("data/labels_cleaned.csv")
 
 X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.3, random_state=25)
+
+def create_sample_json():
+    # export 3 benign and 3 DoS samples in json format
+    # say in the name of the file if it's a benign or a DoS sample (benign_sample_1.json, dos_sample_1.json, benign_sample_2.json, etc.)
+    benign_samples = features[labels['Label'] == 0].sample(3)
+    dos_samples = features[labels['Label'] == 1].sample(3)
+
+    for i in range(3):
+        # use column mappings to get the original column names
+        benign_samples.iloc[i].rename(COLUMN_MAPPINGS).to_json(f"data/sample/benign_sample_{i + 1}.json")
+        dos_samples.iloc[i].rename(COLUMN_MAPPINGS).to_json(f"data/sample/dos_sample_{i + 1}.json")
+
+# create_sample_json()
 
 # Scale data
 if not IS_RANDOM_TREE:
